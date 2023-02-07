b000ec702bca3b119f45290de97e50484d18a9bd
Koki Fushimi <paalon1936@gmail.com>
2023-02-07 16:10:12 +0900

    Add fizzbuzz

---
+++ fizzbuzz/src/julia/fizzbuzz.jl
@@ -0,0 +1,11 @@
+for n in 1:50
+    if n % 15 == 0
+        println("fizz buzz")
+    elseif n % 3 == 0
+        println("fizz")
+    elseif n % 5 == 0
+        println("buzz")
+    else
+        println(n)
+    end
+end
