6760162949160b43e8e520b41c9f35fd825cd341
Koki Fushimi <paalon1936@gmail.com>
2023-02-07 17:21:14 +0900

    Add fizzbuzz

---
+++ fizzbuzz/src/go/fizzbuzz.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	for n := 1; n <= 50; n++ {
+		if n%15 == 0 {
+			fmt.Println("fizz buzz")
+		} else if n%3 == 0 {
+			fmt.Println("fizz")
+		} else if n%5 == 0 {
+			fmt.Println("buzz")
+		} else {
+			fmt.Println(n)
+		}
+	}
+}
