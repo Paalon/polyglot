410a0b41f5de7e7f0ccf2862cbe7c37cf537296a
Koki Fushimi <paalon1936@gmail.com>
2023-02-07 18:08:37 +0900

    Add fizzbuzz.pas

---
+++ fizzbuzz/src/pascal/fizzbuzz.pas
@@ -0,0 +1,16 @@
+program fizzbuzz;
+type
+  range = 1..50;
+var
+  n : range;
+begin
+  for n := low(range) to high(range) do
+    if n mod 15 = 0 then
+      writeln('fizz buzz')
+    else if n mod 3 = 0 then
+      writeln('fizz')
+    else if n mod 5 = 0 then
+      writeln('buzz')
+    else
+      writeln(n)
+end.
