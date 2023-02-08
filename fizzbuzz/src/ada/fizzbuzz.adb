18f391a297df5786178b9a0b7a8fa2e355153056
Koki Fushimi <paalon1936@gmail.com>
2023-02-08 22:29:41 +0900

    Refactoring

--- fizzbuzz/src/ada/fizzbuzz.adb
+++ fizzbuzz/src/ada/fizzbuzz.adb
@@ -1,17 +1,18 @@
 with Ada.Text_IO; use Ada.Text_IO;
+
 procedure FizzBuzz is
-   n : Integer;
+   N : Integer;
 begin
-   n := 1;
-   for n in Integer range 1 .. 50 loop
-      if n mod 3 = 0 and n mod 5 = 0 then
+   N := 1;
+   for N in Integer range 1 .. 50 loop
+      if N mod 3 = 0 and N mod 5 = 0 then
          Put_Line ("fizz buzz");
-      elsif n mod 3 = 0 then
+      elsif N mod 3 = 0 then
          Put_Line ("fizz");
-      elsif n mod 5 = 0 then
+      elsif N mod 5 = 0 then
          Put_Line ("buzz");
       else
-         Put_Line (Integer'Image(n));
+         Put_Line (Integer'Image (N));
       end if;
    end loop;
 end FizzBuzz;
