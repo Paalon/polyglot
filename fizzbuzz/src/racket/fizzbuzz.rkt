1eb6e1608363dd56350da0f9d97653f246c438f0
Koki Fushimi <paalon1936@gmail.com>
2023-02-10 17:58:19 +0900

    Refactoring.

--- fizzbuzz/src/racket/fizzbuzz.rkt
+++ fizzbuzz/src/racket/fizzbuzz.rkt
@@ -1,14 +1,12 @@
 #lang racket
 
-(for ([n (in-range 1 50)])
-    (begin 
-        (cond
-            [(and (eq? (modulo n 3) 0) (eq? (modulo n 5) 0))
-                (display "fizz buzz")]
-            [(eq? (modulo n 3) 0)
-                (display "fizz")]
-            [(eq? (modulo n 5) 0)
-                (display "buzz")]
-            [else
-                (display n)])
-        (newline)))
+(for ([n (in-range 1 51)])
+  (cond
+    [(and (eq? (modulo n 3) 0) (eq? (modulo n 5) 0))
+      (displayln "fizz buzz")]
+    [(eq? (modulo n 3) 0)
+      (displayln "fizz")]
+    [(eq? (modulo n 5) 0)
+      (displayln "buzz")]
+    [else
+      (displayln n)]))
