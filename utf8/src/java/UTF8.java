77f40d736b63a980592e0f1ca72f5933e49b674b
Koki Fushimi <paalon1936@gmail.com>
2023-02-10 16:47:19 +0900

    Refactor utf8 for Java.

--- utf8/src/java/UTF8.java
+++ utf8/src/java/UTF8.java
@@ -1,11 +1,7 @@
 public class UTF8 {
     public static void main(String[] args) {
-        String str = "🍎 and 🍏";
-        int offset = 0, strLen = str.length();
-        while (offset < strLen) {
-            int curChar = str.codePointAt(offset);
-            offset += Character.charCount(curChar);
-            System.out.printf("%c\n", curChar);
+        for (int n: "🍎 and 🍏".codePoints().toArray()) {
+            System.out.printf("%c\n", n);
         }
     }
 }
