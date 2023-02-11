3128ecc1cc2953a7d50e1b404d5b8ac177723d58
Koki Fushimi <paalon1936@gmail.com>
2023-02-11 12:27:54 +0900

    Refactoring.

--- fizzbuzz/src/scheme/fizzbuzz.scm
+++ fizzbuzz/src/scheme/fizzbuzz.scm
@@ -2,18 +2,17 @@
   (scheme base)
   (scheme write))
 
-(define fizzbuzz
-  (lambda (n)
-    (begin
-      (if (> n 1) (fizzbuzz (- n 1)) ())
-      (cond
-        ((and (= (modulo n 3) 0) (= (modulo n 5) 0))
-          (begin (display "fizz buzz") (newline)))
-        ((= (modulo n 3) 0)
-          (begin (display "fizz") (newline)))
-        ((= (modulo n 5) 0)
-          (begin (display "buzz") (newline)))
-        (else
-          (begin (display n) (newline)))))))
-
-(fizzbuzz 50)
+(do
+  ((n 1 (+ n 1)))
+  ((> n 50) ())
+  (begin
+    (cond
+      ((and (= (modulo n 3) 0) (= (modulo n 5) 0))
+        (display "fizz buzz"))
+      ((= (modulo n 3) 0)
+        (display "fizz"))
+      ((= (modulo n 5) 0)
+        (display "buzz"))
+      (else
+        (display n)))
+    (newline)))
