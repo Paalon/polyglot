23522c0d8789dbb9dbcb0ac5a486cfa005f3a0e7
Koki Fushimi <paalon1936@gmail.com>
2023-02-12 10:49:28 +0900

    Add hello for COBOL.

---
+++ hello/src/cobol/hello.cbl
@@ -0,0 +1,11 @@
+       >>SOURCE FORMAT FREE
+identification division.
+program-id. hello.
+environment division.
+configuration section.
+data division.
+working-storage section.
+procedure division.
+    display 'Hello!'.
+    goback.
+end program hello.
