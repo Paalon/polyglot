ef408f4cf513a7541bcced0a7dfdf0d47fc72a3e
Koki Fushimi <paalon1936@gmail.com>
2023-02-18 08:52:45 +0900

    Refactoring.

--- fizzbuzz/src/racket/fizzbuzz.rkt
+++ fizzbuzz/src/racket/fizzbuzz.rkt
@@ -1,12 +1,9 @@
 #lang racket
 
 (for ([n (in-range 1 51)])
-  (cond
-    [(and (eq? (modulo n 3) 0) (eq? (modulo n 5) 0))
-      (displayln "fizz buzz")]
-    [(eq? (modulo n 3) 0)
-      (displayln "fizz")]
-    [(eq? (modulo n 5) 0)
-      (displayln "buzz")]
-    [else
-      (displayln n)]))
+  (displayln
+    (cond
+      [(and (= (modulo n 3) 0) (= (modulo n 5) 0)) "fizz buzz"]
+      [(= (modulo n 3) 0) "fizz"]
+      [(= (modulo n 5) 0) "buzz"]
+      [else n])))
--- fizzbuzz/src/scheme/fizzbuzz.scm
+++ fizzbuzz/src/scheme/fizzbuzz.scm
@@ -6,13 +6,10 @@
   ((n 1 (+ n 1)))
   ((> n 50) ())
   (begin
-    (cond
-      ((and (= (modulo n 3) 0) (= (modulo n 5) 0))
-        (display "fizz buzz"))
-      ((= (modulo n 3) 0)
-        (display "fizz"))
-      ((= (modulo n 5) 0)
-        (display "buzz"))
-      (else
-        (display n)))
+    (display
+      (cond
+        ((and (= (modulo n 3) 0) (= (modulo n 5) 0)) "fizz buzz")
+        ((= (modulo n 3) 0) "fizz")
+        ((= (modulo n 5) 0) "buzz")
+        (else n)))
     (newline)))
