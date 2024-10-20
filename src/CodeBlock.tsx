868fe245ac8f366c16b163fefc9c3a0c909f8b85
Koki Fushimi <paalon1936@gmail.com>
2024-10-20 23:43:14 +0900

    Add `CodeBlock`.

---
+++ src/CodeBlock.tsx
@@ -0,0 +1,23 @@
+import styles from "./CodeBlock.module.css";
+
+import hljs from "highlight.js";
+
+export function CodeBlock({
+	caption = undefined,
+	language,
+	code,
+}: {
+	caption?: string,
+	language: string,
+	code: string,
+}) {
+	/* eslint-disable @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
+	const highlightedCode = hljs.highlight(code, { language }).value as string;
+	return <div className={styles.codeBlock}>
+		{caption !== undefined && <div className={styles.caption}>{caption}</div>}
+		<div className={styles.language}>{language}</div>
+		<div className={`${styles.code} hljs`}>
+			<pre><code className={styles.fontMonospace} dangerouslySetInnerHTML={{__html: highlightedCode }}></code></pre>
+		</div>
+	</div>;
+}
