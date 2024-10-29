44bc0886ab6b3246fdf08348cd62061c1361faa1
Koki Fushimi <paalon1936@gmail.com>
2024-10-30 06:20:56 +0900

    Refactor fizzbuzz for C++.

--- src/program/cplusplus/fizzbuzz/src/main.cpp
+++ src/program/cplusplus/fizzbuzz/src/main.cpp
@@ -1,17 +1,32 @@
-#include <iostream>
+#include <print>
+#include <string>
+#include <format>
+#include <concepts>
 
-using std::cout, std::endl;
+using std::literals::string_literals::operator""s;
+
+template <std::integral T>
+std::string fizzbuzzString(T n) {
+    auto const n3 = n % static_cast<T>(3) == static_cast<T>(0);
+    auto const n5 = n % static_cast<T>(5) == static_cast<T>(0);
+    if (n3 && n5) {
+        return "fizz buzz"s;
+    } else if (n3) {
+        return "fizz"s;
+    } else if (n5) {
+        return "buzz"s;
+    } else {
+        return std::format("{}", n);
+    }
+}
+
+template <std::integral T>
+void printFizzbuzz(T n, T m) {
+    for (auto k = n; k <= m; k++) {
+        std::println("{}", fizzbuzzString(k));
+    }
+}
 
 int main() {
-    for (auto n = 1; n <= 50; n++) {
-        if (n % 3 == 0 && n % 5 == 0) {
-            cout << "fizz buzz" << endl;
-        } else if (n % 3 == 0) {
-            cout << "fizz" << endl;
-        } else if (n % 5 == 0) {
-            cout << "buzz" << endl;
-        } else {
-            cout << n << endl;
-        }
-    }
+    printFizzbuzz(1, 30);
 }
