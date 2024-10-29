e31992ac18046b3e1d3d1070c604811e686281cd
Koki Fushimi <paalon1936@gmail.com>
2024-10-30 03:01:06 +0900

    Arrange indentations.

--- src/program/nim/leibniz/src/leibniz.nim
+++ src/program/nim/leibniz/src/leibniz.nim
@@ -2,16 +2,16 @@ import os
 import strutils
 
 proc powersign(n: int64): float64 =
-    if n mod 2 == 0:
-        return 1.0
-    else:
-        return -1.0
+  if n mod 2 == 0:
+    return 1.0
+  else:
+    return -1.0
 
 proc leibniz(n: int64): float64 =
-    var s = 0.0
-    for i in 0 .. n:
-        s += powersign(i) / (2.0 * i.float64 + 1.0)
-    return 4.0 * s
+  var s = 0.0
+  for i in 0 .. n:
+    s += powersign(i) / (2.0 * i.float64 + 1.0)
+  return 4.0 * s
 
 let n = parseInt(paramStr(1))
 echo leibniz(n)
