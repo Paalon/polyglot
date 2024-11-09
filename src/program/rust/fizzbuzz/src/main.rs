28d55defed6e1487b0d3a0905cb1dc25a865916c
Koki Fushimi <paalon1936@gmail.com>
2024-11-10 00:23:37 +0900

    Refactor.

--- src/program/rust/fizzbuzz/src/main.rs
+++ src/program/rust/fizzbuzz/src/main.rs
@@ -1,13 +1,18 @@
-fn main() {
-    for n in 1..=50 {
-        if n % 3 == 0 && n % 5 == 0 {
-            println!("fizz buzz");
-        } else if n % 3 == 0 {
-            println!("fizz");
-        } else if n % 5 == 0 {
-            println!("buzz");
-        } else {
-            println!("{}", n);
-        }
+fn fizz_buzz_element_string(n: i64) -> String {
+    match (n % 3 == 0, n % 5 == 0) {
+        (true, true) => "fizz buzz".to_string(),
+        (true, _) => "fizz".to_string(),
+        (_, true) => "buzz".to_string(),
+        _ => n.to_string(),
     }
+}
+
+fn print_fizz_buzz(n: i64) {
+    for k in 1..=n {
+        println!("{}", fizz_buzz_element_string(k))
+    }
+}
+
+fn main() {
+    print_fizz_buzz(30)
 }
